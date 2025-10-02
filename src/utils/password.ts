6cd9ce881afc98e2dcd19607a120d0d3b8e2df09
SamuelMoyaVegamonte <samuelmoya786@gmail.com>
2025-10-02 00:18:42 -0400

    implemento password

---
+++ src/utils/password.ts
@@ -0,0 +1,12 @@
+// utils/password.ts
+import bcrypt from 'bcrypt';
+
+const ROUNDS = 10;
+
+export async function hashPassword(plain: string): Promise<string> {
+  return bcrypt.hash(plain, ROUNDS);
+}
+
+export async function comparePassword(plain: string, hash: string): Promise<boolean> {
+  return bcrypt.compare(plain, hash);
+}
