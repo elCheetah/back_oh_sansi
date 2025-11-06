d2f7d705ec817d894ba734e5d784b6b7015b2f4e
SamuelMoyaVegamonte <samuelmoya786@gmail.com>
2025-11-06 17:58:26 -0400

    Creacion de archivos.

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,18 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/config/database.ts
+exports.prisma = void 0;
 const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-// (Opcional) Conectar para verificar al inicio
-async function connectDB() {
+const globalForPrisma = global;
+exports.prisma = globalForPrisma.prisma ||
+    new client_1.PrismaClient({
+        log: ['query', 'warn', 'error'],
+    });
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = exports.prisma;
+}
+(async () => {
     try {
-        await prisma.$connect();
-        console.log('✅ Conexión a la base de datos exitosa');
+        await exports.prisma.$connect();
+        console.log('✅ Conexión establecida con la base de datos');
     }
     catch (error) {
-        console.error('❌ Error de conexión a la base de datos:', error);
-        process.exit(1);
+        console.error('❌ Error al conectar Prisma:', error);
     }
-}
-connectDB();
-exports.default = prisma;
+})();
+exports.default = exports.prisma;
