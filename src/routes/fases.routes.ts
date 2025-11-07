358284a30f1d66c6aa19f91b671c57d358f67b61
SamuelMoyaVegamonte <samuelmoya786@gmail.com>
2025-11-07 16:41:25 -0400

    Correccion en rutas

--- src/routes/fases.routes.ts
+++ src/routes/fases.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { FasesController } from "../controllers/fases.controller";
-import { requireAdmin } from "../middlewares/requireAdmin";
+// import { requireAdmin } from "../middlewares/requireAdmin";
 
 const router = Router();
 
-router.get("/fases/estado", requireAdmin, FasesController.estado);
-router.post("/fases/abrir", requireAdmin, FasesController.abrir);
-router.post("/fases/cerrar", requireAdmin, FasesController.cerrar);
-router.post("/fases/publicar", requireAdmin, FasesController.publicar);
-router.get("/fases/historial", requireAdmin, FasesController.historial);
+
+router.get("/fases/estado", FasesController.estado);
+router.post("/fases/abrir", FasesController.abrir);
+router.post("/fases/cerrar", FasesController.cerrar);
+router.post("/fases/publicar", FasesController.publicar);
+router.get("/fases/historial", FasesController.historial);
 
 export default router;
